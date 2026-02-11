597a6e3d43ae8da74d67d9e4614abf99168bab62
Phusit Boonwong <phusitboonwong.phu@gmail.com>
2026-02-11 20:48:18 +0700

    new api

--- app/firebaseConfig.ts
+++ app/firebaseConfig.ts
@@ -1,9 +1,8 @@
-// Import the functions you need from the SDKs you need
+// app/firebaseConfig.ts
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// --- เอาค่า Config จาก Firebase Console มาใส่ตรงนี้ ---
 const firebaseConfig = {
   apiKey: "AIzaSyBn92n3tFAXjSle2m5k8Uty5OLibtURN0k",
   authDomain: "my-grade-site.firebaseapp.com",
@@ -12,6 +11,9 @@ const firebaseConfig = {
   messagingSenderId: "819387558522",
   appId: "1:819387558522:web:a358a29c0043e22ea8a9c3"
 };
+// ---------------------------------------------------
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// *** สำคัญมาก! ต้องมีบรรทัดนี้ และต้องมีคำว่า export ***
+export const db = getFirestore(app);
